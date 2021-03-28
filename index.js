de3416930093a58e75654954252832996943c222
Spycsh <39623753+Spycsh@users.noreply.github.com>
2021-03-29 00:25:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,17 +35,28 @@ const diffUrl = github.context.payload.pull_request.diff_url;   // get the diffe
 // console.log("diff:");
 // console.log(diffUrl);
 
+var HOST_NAME = 'patch-diff.githubusercontent.com';
+var options = { 
+    host: HOST_NAME,
+    path: diffUrl.substr(HOST_NAME.length),   // the remaining file path under the host
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        'Accept-Charset': 'utf-8;q=1',
+        'Accept-Encoding': '*;q=0',
+        'Connection':'close'
+      },
+      agent: false,
+      method: 'GET',
+
+};
+
 https.get(diffUrl,function(res){
   var str = "";
+  console.log('Response is '+res.statusCode);
   res.on("data",function(chunk){
       str += chunk;
   })
   res.on("end",function(){
-      var data = getData(str);
-      console.log(data);
+      console.log(str);
   })
 })
-
-function getData(str){
-    return str;
-}
