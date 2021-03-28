b79acd2db8ec93171e61406b85989001c051dc0d
Spycsh <39623753+Spycsh@users.noreply.github.com>
2021-03-29 00:31:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ var options = {
 
 };
 
-https.get(diffUrl,function(res){
+https.get(options,function(res){
   var str = "";
   console.log('Response is '+res.statusCode);
   res.on("data",function(chunk){
