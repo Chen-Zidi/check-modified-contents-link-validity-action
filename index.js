8a66edce4bbb8254af34a00f97679ef1a6a22d01
Spycsh <39623753+Spycsh@users.noreply.github.com>
2021-03-29 22:26:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,12 +108,30 @@ async function checkValidUrl(url){
 
         // get the PR status
         // if not close then set close with a msg about that error?
-        
+        const context = github.context;
+        let token = process.env["GITHUB_TOKEN"] || "";
+        if (token === "") {
+          token = core.getInput("github_token");
+        }
+        const client = new github.GitHub(token);
+        core.info("Updating the state of a pull request to closed");
+      
+        // create comment on PR
+        const new_comment = client.issues.createComment({
+          ...context.repo,
+          issue_number: context.payload.pull_request.number,
+          body: "Some invalid urls!"
+        });
+      
+        // update to closed
+        await client.pulls.update({
+          ...context.repo,
+          pull_number: context.issue.number,
+          state: "closed"
+        });
+        core.info(`Closed a pull request ${context.issue.number}`);
     });
 
 }
-
-console.log("ewrfer");
-
 //set output validity
 core.setOutput("validity", true);
